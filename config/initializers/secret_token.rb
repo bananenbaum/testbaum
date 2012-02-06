4d1358618ae44ed07234f5733cbb2568846d6e6c
To <to@bananenbaum.com>
2012-02-06 12:53:13 +0100

    Erster Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testbaum::Application.config.secret_token = '48a6ee29476984f23a6b5c153e52be0dc4ed67adffe05fc389c359020fd7b076b628f58b47eec088a132194d25d69a9e00e1b58127a7a376d8f6f647a97537e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Testbaum::Application.config.session_store :cookie_store, key: '_testbaum_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Testbaum::Application.config.session_store :active_record_store
